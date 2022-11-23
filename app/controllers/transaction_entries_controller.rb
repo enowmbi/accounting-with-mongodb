5ee7ab1638cc3799c8b935720070010db11b5b9c
Enow Mbi <benowmbi@yahoo.com>
2022-11-23 14:15:00 +0100

    Add total credits and total debits

--- app/controllers/transaction_entries_controller.rb
+++ app/controllers/transaction_entries_controller.rb
@@ -2,12 +2,16 @@ class TransactionEntriesController < ApplicationController
   def create
     @transaction = Transaction.find(params[:id])
     @transaction.transaction_entries.build(transaction_entry_params)
+    @transaction.total_debits += params[:transaction_entry][:amount].to_f if params[:transaction_entry][:entry_type] == "Debit"
+    @transaction.total_credits += params[:transaction_entry][:amount].to_f if params[:transaction_entry][:entry_type] == "Credit"
     respond_to do |format|
       if @transaction.save
         format.turbo_stream do
           render turbo_stream: [
             turbo_stream.update("new-transaction-entry-form", partial: "transaction_entries/form", locals: { transaction_entry: TransactionEntry.new }),
-            turbo_stream.prepend("transaction_entries", partial: "transaction_entries/transaction_entry", locals: { transaction_entry: @transaction.transaction_entries.last }),
+            turbo_stream.prepend("transaction-entries", partial: "transaction_entries/transaction_entry", locals: { transaction_entry: @transaction.transaction_entries.last }),
+            turbo_stream.update("total-credits", html: @transaction.total_credits),
+            turbo_stream.update("total-debits", html: @transaction.total_credits),
             turbo_stream.update("notice", html: "Transaction Entry was successfully")
           ]
         end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,6 +6,8 @@ class Transaction
   field :memo, type: String
   field :ref, type: String
   field :balanced, type: Mongoid::Boolean
+  field :total_debits, type: BigDecimal, default: 0.00
+  field :total_credits, type: BigDecimal, default: 0.00
 
   embeds_many :transaction_entries
 end
